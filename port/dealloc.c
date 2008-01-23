f194fdd9d1d22a5b9d4cde998cbc3c16da530234
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2008-01-23 19:47:51 +0000

    hunting the leak
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/mdwf/trunk@392 65d2813f-f11f-0410-b17b-8647a28db151

--- port/dealloc.c
+++ port/dealloc.c
@@ -3,13 +3,17 @@
 void
 q(free)(struct Q(State) *state, void *ptr, size_t bytes)
 {
-  if (ptr == NULL)
-    return;
-
-  free(ptr);
-
-  if (state) {
-    state->allocated -= bytes;
-    Q(fini)(&state);
-  }
+    if (ptr == NULL)
+	return;
+    
+    free(ptr);
+    
+    if (state) {
+	printf("%s(%p,%d): state=%p, used=%d, saved=%d\n",
+	       __FUNCTION__, ptr, (int)bytes,
+	       state,
+	       state->used, state->saved);
+	state->allocated -= bytes;
+	Q(fini)(&state);
+    }
 }
