a5fd69e89849ae738b5a9498c603030e0f3b273d
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2008-04-15 23:45:17 +0000

    main lib a-tests done.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/mdwf/trunk@529 65d2813f-f11f-0410-b17b-8647a28db151

---
+++ tests/parts/test-op-ax1ax-3.c
@@ -0,0 +1,126 @@
+#include <qop-mdwf3.h>
+#include "../../port/mdwf.h"
+#include "opxtest.h"
+#include "op-routines.h"
+
+char *op_a_name = "1";
+char *op_b_name = "inverse(mlib:conj(A)) * mlib:conj(A)";
+
+double
+read_gauge(int dir,
+	   const int pos[4],
+	   int a, int b,
+	   int re_im,
+	   void *env)
+{
+    int i;
+    unsigned int v = sum_init(seed_u);
+
+    v = sum_add(v, re_im);
+    v = sum_add(v, a);
+    v = sum_add(v, b);
+    for (i = 0; i < 4; i++) {
+	v = sum_add(v, pos[i]);
+	v = sum_add(v, dir);
+    }
+    v = sum_add(v, seed_u);
+    return sum_fini(v);
+}
+
+static double
+read_fermion(unsigned int seed,
+	     const int pos[5],
+	     int c, int d,
+	     int re_im)
+{
+    int i;
+    unsigned int v = sum_init(seed);
+    v = sum_add(v, c);
+    v = sum_add(v, d);
+    v = sum_add(v, re_im);
+    for (i = 0; i < 5; i++)
+	v = sum_add(v, pos[i]);
+    v = sum_add(v, seed);
+    return sum_fini(v);
+}
+
+double
+read_fermion_a(const int pos[5],
+	       int c, int d,
+	       int re_im,
+	       void *env)
+{
+    return read_fermion(seed_a, pos, c, d, re_im);
+}
+
+double
+read_fermion_b(const int pos[5],
+	       int c, int d,
+	       int re_im,
+	       void *env)
+{
+    return read_fermion(seed_b, pos, c, d, re_im);
+}
+
+int
+operator_a(void)
+{
+    double x, y;
+
+    dot_fermion(&x, &y, fermion_b, fermion_a);
+
+    zprint("dot     : %20.10e %20.10e", x, y);
+    return 0;
+}
+
+static void
+Ax(struct QX(Fermion) *r,
+   const struct Q(Parameters) *params,
+   const struct QX(Fermion) *s)
+{
+    struct Q(State) *state = params->state;
+    long long flops = 0;
+
+    qx(op_Ax)(r->even, &state->even, params, s->even, &flops);
+    qx(op_Ax)(r->odd, &state->odd, params, s->odd, &flops);
+	      
+}
+
+static void
+A1x(struct QX(Fermion) *r,
+    const struct Q(Parameters) *params,
+    const struct QX(Fermion) *s)
+{
+    struct Q(State) *state = params->state;
+    long long flops = 0;
+
+    qx(op_A1x)(r->even, &state->even, params, s->even, &flops);
+    qx(op_A1x)(r->odd, &state->odd, params, s->odd, &flops);
+}
+
+
+int
+operator_b(void)
+{
+    double x, y;
+    struct QX(Fermion) *fermion_x;
+    struct QX(Fermion) *fermion_y;
+
+    if (QOP_MDWF_allocate_fermion(&fermion_x, gauge->state)) {
+	zprint("operator_1/A A (): alloc 1 failed");
+	return 1;
+    }
+
+    if (QOP_MDWF_allocate_fermion(&fermion_y, gauge->state)) {
+	zprint("operator_1/A A (): alloc 2 failed");
+	return 1;
+    }
+
+    Ax(fermion_x, params, fermion_a);
+    A1x(fermion_y, params, fermion_x);
+    dot_fermion(&x, &y, fermion_b, fermion_y);
+    QOP_MDWF_free_fermion(&fermion_x);
+    QOP_MDWF_free_fermion(&fermion_y);
+    zprint("identity: %20.10e %20.10e", x, y);
+    return 0;
+}
