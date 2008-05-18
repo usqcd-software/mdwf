e033db362a947fe36fe3a18872a33b228c18c4ee
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2008-05-18 01:46:41 +0000

    Release 1.1.4
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/mdwf/releases/mdwf-1.1.4@607 65d2813f-f11f-0410-b17b-8647a28db151

--- port/version.c
+++ port/version.c
@@ -1,11 +1,11 @@
 #include <mdwf.h>
 
 /* Return version of the MDWF library
- *  2007/10/09  working copy
+ *  2008/05/17 Release 1.1.4
  */
 
 const char *
 Q(version)(void)
 {
-  return "XXX working copy";
+  return "Release 1.1.4";
 }
