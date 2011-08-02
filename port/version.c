4a221dafc3e871ef5c814db670f6e3cb3a99bacb
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2011-08-02 00:13:32 +0000

    Release 1.3.3 frozen.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/mdwf/releases/mdwf-1.3.3@2235 65d2813f-f11f-0410-b17b-8647a28db151

--- port/version.c
+++ port/version.c
@@ -1,11 +1,10 @@
 #include <mdwf.h>
 
 /* Return version of the MDWF library
- *  2007/10/09  working copy
  */
 
 const char *
 Q(version)(void)
 {
-  return "Version 1.3.0 XXX $Id$";
+  return "Version 1.3.3 $Id$";
 }
