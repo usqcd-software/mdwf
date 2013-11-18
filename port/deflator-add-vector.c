979284db507df9ad6a7387f426200268aadc4511
Sergey Syritsyn <syritsyn@alum.mit.edu>
2013-11-18 13:21:23 -0500

    bug in deflator_add_vector fixed ; debug out rm'd also changed deflator_add_vector to avoid changing the added vector

--- port/deflator-add-vector.c
+++ port/deflator-add-vector.c
@@ -1,12 +1,14 @@
 #define QOP_MDWF_DEFAULT_PRECISION 'F'
 #include <mdwf.h>
 
+
 int
 QX(deflator_add_vector)(const struct Q(Parameters)  *params,
                         const struct QX(Gauge)      *gauge,
                         struct Q(Deflator) *deflator,
                         const struct QX(HalfFermion) *hfermion)
 {
+#define cur_v       (deflator->work_c_1)
   DECLARE_STATE;
   long long flops = 0;
   long long sent = 0;
@@ -40,17 +42,20 @@ QX(deflator_add_vector)(const struct Q(Parameters)  *params,
   ws.params    = params;
   ws.gauge     = gauge->data;
   ws.tmp_e     = temps;
-  ws.tmp2_e    = qx(step_even)(state, temps);
-  ws.tmp_o     = qx(step_even)(state, temps);
+  ws.tmp2_e    = temps = qx(step_even)(state, temps);
+  ws.tmp_o     = temps = qx(step_even)(state, temps);
   ws.flops     = &flops;
   ws.sent      = &sent;
   ws.received  = &received;
 
   lv = q(latvec_c_view)(deflator->dim, deflator->Ls, hfermion->even);
-  q(df_inject)(deflator, &ws, lv);
+  /* q(df_inject) modifies the injected vector, so copy it */
+  q(latvec_c_copy)(lv, cur_v);
+  q(df_inject)(deflator, &ws, cur_v);
 
   END_TIMING(state, flops, sent, received);
   if (ptr)
     q(free)(state, ptr, ptr_size);
   return 0;
+#undef cur_v
 }
